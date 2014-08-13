745ce6fefb9cb788903bb799f86c75a0cc0de3e5
ZhangZheXu <zhangzhexu@ZhangZheXus-Mac.local>
2014-08-13 02:36:05 -0700

    Initial Commit

---
+++ ShipFile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ShipFile
+//
+//  Created by ZhangZheXu on 14-8-13.
+//  Copyright (c) 2014年 ZhangZheXu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
